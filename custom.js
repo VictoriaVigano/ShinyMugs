8217684adafe5ae5dd2cf8fa099074d24d0cee09
VictoriaVigano <91489683+VictoriaVigano@users.noreply.github.com>
2022-01-10 18:55:51 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -100,3 +100,56 @@ function loadHtml(){
 function clearHtml(){
     containerBuyCart.innerHTML = '';
 }
+
+function store(theForm) {
+    document.getElementById('welcomeMessage').innerHTML = "";
+    var inputUsername= theForm["username"];
+    var inputPassword= theForm["password"];
+    localStorage.setItem("username", inputUsername.value);
+    localStorage.setItem("password", inputPassword.value);
+    document.getElementById('welcomeMessage').innerHTML = "Welcome " + localStorage.getItem('username') + "!";
+    return false;
+    }
+
+
+    function login(theForm) {
+    document.getElementById('welcomeMessage').innerHTML = "";
+    var inputUsername = theForm["username"];
+    var inputPassword = theForm["password"];
+    var username = inputUsername.value;
+    var password = inputPassword.value;
+    if ((username == localStorage.getItem('username')) && (password == localStorage.getItem('password'))) {
+        document.getElementById('welcomeMessage').innerHTML = "Welcome " + localStorage.getItem('username') + "!";
+        } else {
+    document.getElementById('welcomeMessage').innerHTML = "Invalid Log-in!";
+    }
+    return false;
+    } 
+
+$.ajax({
+        url: 'https://api.email-validator.net/api/verify',
+        type: 'POST',
+        cache: false,
+        crossDomain: true,
+        data: { EmailAddress: 'email address', APIKey: 'your API key' },
+        dataType: 'json',
+        success: function (json) {
+            // check API result
+            if (typeof(json.status) != "undefined") {
+                var resultcode = json.status;
+                if (typeof(json.info) != "undefined") {
+                    // short summary
+                    info = json.info;
+                } else info = "";
+                if (typeof(json.details) != "undefined") {
+                    // detailed description
+                    details = json.details;
+                } else details = "";
+                // resultcode 200, 207, 215 - valid
+                // resultcode 215 - can be retried to update catch-all status
+                // resultcode 114 - greylisting, wait 5min and retry
+                // resultcode 118 - api rate limit, wait 5min and retry
+                // resultcode 3xx/4xx - bad
+            }
+        }
+    })
